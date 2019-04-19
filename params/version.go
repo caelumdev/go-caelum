e28ce880a0f812710b67e4b0554e323f47a23dcf
caelumdev <walschot.vanstalle@gmail.com>
2019-04-19 11:09:28 +0200

    Add DEV tags + version

--- params/version.go
+++ params/version.go
@@ -22,9 +22,9 @@ import (
 
 const (
 	VersionMajor = 1        // Major version component of the current release
-	VersionMinor = 3        // Minor version component of the current release
-	VersionPatch = 2        // Patch version component of the current release
-	VersionRevision = 1			// Revision
+	VersionMinor = 0        // Minor version component of the current release
+	VersionPatch = 0        // Patch version component of the current release
+	VersionRevision = ipfs-1			// Revision
 	VersionMeta  = "clmp-dev" // Version metadata to append to the version string
 )
 
