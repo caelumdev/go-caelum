3db4958133ae70614a89f3f0580dba335d77d6d4
caelumdev <43523781+caelumdev@users.noreply.github.com>
2019-08-18 21:57:11 +0200

    Set version to 1.1.0 mainnet

--- params/version.go
+++ params/version.go
@@ -22,9 +22,9 @@ import (
 
 const (
 	VersionMajor = 1        // Major version component of the current release
-	VersionMinor = 4        // Minor version component of the current release
+	VersionMinor = 1        // Minor version component of the current release
 	VersionPatch = 0        // Patch version component of the current release
-	VersionMeta  = "mainnet-alpha" // Version metadata to append to the version string
+	VersionMeta  = "mainnet" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
